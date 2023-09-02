84a1bbc89ad8804c1c2f7e718435ce38fb72e7dc
karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-09-02 18:59:27 -0400

    senhoooor

--- js/index.js
+++ js/index.js
@@ -5,58 +5,56 @@ window.onload = () => {
     }
   };
 
+
   const cells = document.querySelectorAll('.cell');
-  let currentPlayer = 'palmeiras';
-  let gameEnded = false;
-  
-  // Função para verificar o vencedor
-  function checkWinner() {
-      const winningCombinations = [
-          [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-          [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-          [0, 4, 8], [2, 4, 6]            // Diagonais
-      ];
-  
-      for (const combination of winningCombinations) {
-          const [a, b, c] = combination;
-          if (cells[a].classList.contains(currentPlayer) && cells[b].classList.contains(currentPlayer) && cells[c].classList.contains(currentPlayer)) {
-              cells[a].classList.add('winning-cell');
-              cells[b].classList.add('winning-cell');
-              cells[c].classList.add('winning-cell');
-              return true;
-          }
-      }
-  
-      return false;
-  }
-  
-  // Função para reiniciar o jogo
-  function resetGame() {
-      cells.forEach(cell => {
-          cell.classList.remove('palmeiras', 'corinthians', 'winning-cell');
-      });
-  
-      gameEnded = false;
-      currentPlayer = 'palmeiras';
-  }
-  
-  cells.forEach(cell => {
-      cell.addEventListener('click', () => {
-          if (!gameEnded && !cell.classList.contains('palmeiras') && !cell.classList.contains('corinthians')) {
-              cell.classList.add(currentPlayer);
-  
-              if (checkWinner()) {
-                  gameEnded = true;
-                  alert(`O time do ${currentPlayer === 'palmeiras' ? 'Palmeiras' : 'Corinthians'} venceu!`);
-                  resetGame();
-              } else if ([...cells].every(cell => cell.classList.contains('palmeiras') || cell.classList.contains('corinthians'))) {
-                  gameEnded = true;
-                  alert("Empate!");
-                  resetGame();
-              } else {
-                  currentPlayer = currentPlayer === 'palmeiras' ? 'corinthians' : 'palmeiras';
-              }
-          }
-      });
-  });
-  
+let currentPlayer = 'X';
+let gameEnded = false;
+
+// Função para verificar o vencedor
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+        [0, 4, 8], [2, 4, 6]            // Diagonais
+    ];
+
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent) {
+            return cells[a].textContent;
+        }
+    }
+
+    return null;
+}
+
+// Função para reiniciar o jogo
+function resetGame() {
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+
+    gameEnded = false;
+    currentPlayer = 'X';
+}
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (!gameEnded && !cell.textContent) {
+            cell.textContent = currentPlayer;
+
+            const winner = checkWinner();
+            if (winner) {
+                gameEnded = true;
+                alert(`Jogador ${winner} venceu!`);
+                resetGame();
+            } else if ([...cells].every(cell => cell.textContent !== '')) {
+                gameEnded = true;
+                alert("Empate!");
+                resetGame();
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        }
+    });
+});
